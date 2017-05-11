3e6ab54d7f4abd777e25fb134297fe7145722329
Morgan <quixoticbibliomania@gmail.com>
2017-05-11 11:59:07 -0400

    import cleanup

--- Connection-Manager/src/client/DefaultClientGUI.java
+++ Connection-Manager/src/client/DefaultClientGUI.java
@@ -7,11 +7,7 @@ import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
 import java.io.PrintStream;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Queue;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
